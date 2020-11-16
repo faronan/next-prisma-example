3b735bb2f421140d859f5da2be617f96573c27ea
faronan <toshiki.ito88@gmail.com>
2020-11-17 08:54:19 +0900

    適当な差分その2(CLIでvercel deployした時)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Test Vercel CI</title>
+        <title>Test Vercel CI by CLI</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
