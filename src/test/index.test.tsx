c5607cdbd9dc4d50c4b732181226d150aece42f3
faronan <toshiki.ito88@gmail.com>
2020-11-19 12:58:45 +0900

    コンポーネントのテストコードを追加

---
+++ src/test/index.test.tsx
@@ -0,0 +1,9 @@
+import {shallow} from "enzyme";
+import Home from '../pages/index';
+
+describe("Home", () => {
+  test("check h1 tag's text", () => {
+    const wrapper = shallow(<Home />);
+    expect(wrapper.find("h1").text()).toEqual("Welcome to Next.js!");
+  });
+});
