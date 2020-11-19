55f18134570a25b29e7576a2ff0f6cd3cef37383
faronan <toshiki.ito88@gmail.com>
2020-11-19 15:26:07 +0900

    cssとtsxをテストできるように設定ファイルを追加

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,17 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   setupFilesAfterEnv: ['./src/test/setup.ts'],
+  moduleNameMapper: { "\\.(css|scss)$": "identity-obj-proxy" },
+  moduleFileExtensions: [
+    'ts',
+    'tsx',
+    'js',
+    'jsx',
+    'json',
+    'node'
+  ],
+  transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(ts|tsx)$'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'babel-jest',
+  }
 };
