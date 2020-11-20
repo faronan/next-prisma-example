8cc5fd8a573c397774cad50c3451a2fbdfa05bbc
faronan <toshiki.ito88@gmail.com>
2020-11-21 07:50:11 +0900

    jest configにも絶対パスのエイリアスを記載

--- jest.config.ts
+++ jest.config.ts
@@ -4,7 +4,11 @@ const config: Config.InitialOptions = {
   preset: "ts-jest",
   testEnvironment: "node",
   setupFilesAfterEnv: ["./test/setup.ts"],
-  moduleNameMapper: { "\\.(css|scss)$": "identity-obj-proxy" },
+  moduleNameMapper: {
+    "\\.(css|scss)$": "identity-obj-proxy",
+    "^@/(.+)": "<rootDir>/src/$1",
+    "^@test/(.+)": "<rootDir>/test/$1",
+  },
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
   transformIgnorePatterns: ["[/\\\\]node_modules[/\\\\].+\\.(ts|tsx)$"],
   transform: {
